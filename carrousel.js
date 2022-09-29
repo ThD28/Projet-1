2f390022421fb3bf5aac1ce5916a2a09b6485adb
Theo DuboisE <th.dubois201@gmail.comm>
2022-09-29 11:36:24 +0200

    images mises en place format tel

--- carrousel.js
+++ carrousel.js
@@ -2,14 +2,14 @@ const slidesContainer = document.getElementById("slides-container");
 const slide = document.querySelector(".slide");
 const prevButton = document.getElementById("slide-arrow-prev");
 const nextButton = document.getElementById("slide-arrow-next");
- 
-nextButton.addEventListener("click", function() {
-  const slideWidth = slide.clientWidth;
-  console.log(slideWidth);
-  slidesContainer.scrollLeft += slideWidth;
+
+nextButton.addEventListener("click", function () {
+    const slideWidth = slide.clientWidth;
+    console.log(slideWidth);
+    slidesContainer.scrollLeft += slideWidth;
 });
- 
-prevButton.addEventListener("click", function() {
-  const slideWidth = slide.clientWidth;
-  slidesContainer.scrollLeft -= slideWidth;
+
+prevButton.addEventListener("click", function () {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft -= slideWidth;
 });
